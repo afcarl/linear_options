ea42e250c70e830c1d0e8e743020f5956cd5799d
pierre-luc <pierre-luc@pierre-luc-laptop.(none)>
2012-05-03 23:26:50 -0400

    Properly initializing theta vectors.

--- src/LinearQ0Learner.cc
+++ src/LinearQ0Learner.cc
@@ -1,28 +1,48 @@
 #include <linear_options/LinearQ0Learner.hh>
+#include <linear_options/serialization.hh>
 
 using namespace rl;
 
+LinearQ0Learner::LinearQ0Learner(unsigned numActions, double alpha, double epsilon, double gamma, rl::state_abstraction& abstraction, Random rng) : 
+        numActions(numActions), 
+        alpha(alpha),
+        epsilon(epsilon),
+        gamma(gamma),
+        stateAbstraction(&abstraction),
+        rng(rng)
+{ 
+    actionValueThetas.resize(numActions); 
+    for (auto it = actionValueThetas.begin(); it != actionValueThetas.end(); it++) {
+        (*it) = Eigen::VectorXd::Zero(stateAbstraction->length());
+    }
+}
+
 int LinearQ0Learner::getBestAction(const Eigen::VectorXd& phi) 
 {
-    double maxValue;
-    double maxAction = -1*std::numeric_limits<double>::max();
+    double maxAction = 0;
+    double maxValue = -1*std::numeric_limits<double>::max();
     for (unsigned i = 0; i < actionValueThetas.size(); i++) {
+       double value = actionValueThetas[i].dot(phi);
        if (actionValueThetas[i].dot(phi) > maxValue) {
            maxAction = i;
+           maxValue = value;
        }
     }
 
     return maxAction;
 }
 
+int LinearQ0Learner::epsilonGreedy(const Eigen::VectorXd& phi)
+{
+    lastAction  = (rng.uniform() < epsilon) ? rng.uniformDiscrete(0, numActions-1) : getBestAction(phi); 
+    lastPhi = phi;
+    return lastAction;
+}
+
 int LinearQ0Learner::first_action(const std::vector<float> &s)
 {
     auto phi = project(s);
-
-    lastAction  = (rng.uniform() < epsilon) ? rng.uniformDiscrete(0, numActions-1) : getBestAction(phi); 
-    lastPhi = phi;
-
-    return lastAction;
+    return epsilonGreedy(phi);
 }
 
 int LinearQ0Learner::next_action(float reward, const std::vector<float> &s)
@@ -31,19 +51,28 @@ int LinearQ0Learner::next_action(float reward, const std::vector<float> &s)
 
     actionValueThetas[lastAction] = actionValueThetas[lastAction].array() + lastPhi.array()*(reward + gamma*actionValueThetas[getBestAction(phiPrime)].dot(phiPrime) - actionValueThetas[lastAction].dot(lastPhi))*alpha;
 
-    lastAction  = (rng.uniform() < epsilon) ? rng.uniformDiscrete(0, numActions-1) : getBestAction(phiPrime); 
-    lastPhi = phiPrime;
-
-    return lastAction;
+    return epsilonGreedy(phiPrime);
 }
 
 void LinearQ0Learner::last_action(float reward)
 {
-    // TODO Check if this is correct
     actionValueThetas[lastAction] = actionValueThetas[lastAction].array() + lastPhi.array()*(reward - actionValueThetas[lastAction].dot(lastPhi))*alpha;
 }
 
 void LinearQ0Learner::setDebug(bool d) 
 {
 
+}
+void LinearQ0Learner::savePolicy(const std::string& filename)
+{
+    std::ofstream file(filename); 
+    boost::archive::text_oarchive oa(file);
+    oa << actionValueThetas;
+}
+
+void LinearQ0Learner::loadPolicy(const std::string& filename)
+{
+    std::ifstream ifs(filename, std::ios::binary);
+    boost::archive::text_iarchive ia(ifs);
+    ia >> actionValueThetas;
 }
