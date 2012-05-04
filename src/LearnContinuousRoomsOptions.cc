a99ba3af357924be48868ba008fedaffcb398ff8
pierre-luc <pierre-luc@pierre-luc-laptop.(none)>
2012-05-04 14:42:08 -0400

    Defined pseudo-reward decorators and linear options for reaching nearest color.

--- src/LearnContinuousRoomsOptions.cc
+++ src/LearnContinuousRoomsOptions.cc
@@ -1,6 +1,7 @@
 #include <linear_options/StateAbstraction.hh>
 #include <linear_options/ContinuousRooms.hh>
 #include <linear_options/LinearQ0Learner.hh>
+#include <linear_options/RewardDecorator.hh>
 
 /**
  * We build the feature vector from a set of radial basis functions
@@ -40,6 +41,52 @@ private:
     Eigen::DiagonalMatrix<double, 3, 3> C;
 };
 
+/**
+ * Subclasses the LinearOptions to specify the termination set
+ */
+struct ReachNearestStateOfColor : public rl::LinearOption
+{
+    ReachNearestStateOfColor(int targetColor) : targetColor(targetColor) {};
+    int targetColor;
+
+    /**
+     * @Override
+     */
+    double beta(const Eigen::VectorXd& s) 
+    {
+        // Check if the bit for target color is set
+        if (s[targetColor]) {
+            return 1;
+        }
+        return 0;
+    }
+};
+
+/**
+ * This class acts as a decorator that defines its 
+ * own pseudo-reward function over the one returned by 
+ * the actual environment. 
+ */
+struct ReachNearestColorRewardDecorator : public rl::RewardDecorator 
+{
+    ReachNearestColorRewardDecorator(Agent& agent, int targetColor) : 
+        rl::RewardDecorator(agent), 
+        targetColor(targetColor) {};
+
+    int targetColor;
+    static const double REWARD_SUCCESS = 1;
+    static const double REWARD_FAILURE = -0.1;
+
+    double pseudoReward(float reward, const std::vector<float> &s)
+    {
+        if (s[targetColor]) { 
+            return REWARD_SUCCESS;
+        } else {
+            return REWARD_FAILURE;
+        }    
+    }
+};
+
 int main(void)
 {
 // Radial-basis functions are placed every 10 units in 
@@ -59,8 +106,14 @@ for (double x = 10.2/2.0; x < 200; x += 10) {
 Eigen::Vector3d C(1.0/10.2, 1.0/10.2, 1/30);
 room_abstraction stateAbstraction(U.transpose(), C, 200);
 
-ContinuousRooms env("map.png", 5);
-rl::LinearQ0Learner linearQ0Learner(env.getNumActions(), 5e-4, 0.1, 0.6, stateAbstraction);
+const double robotRadius = 5;
+ContinuousRooms env("map.png", robotRadius);
+
+// Learn a policy for reaching the subgoals defined by the pseudo-reward functions
+std::vector<Agent*> agents;
+for (int color = 0; color < ContinuousRooms::NUM_COLORS; color++) {
+    agents.push_back(new ReachNearestColorRewardDecorator(*(new rl::LinearQ0Learner(ContinuousRooms::NUM_ACTIONS, 5e-4, 0.1, 0.6, stateAbstraction)), color));
+}
 
 return 0;
-};
+}
